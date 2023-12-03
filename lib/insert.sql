857c6145c1d40edcc83dec22ce818d8c5580f970
Your Name <mikachupichu@gmail.com>
2023-12-03 12:19:19 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 15, "M", "black", "happy", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 14, "M", "white", "angry", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 5, "F", "black", "rude", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 45, "M", "brown", "tired", 0);
+INSERT INTO bears VALUES (5,"Melissa", 1, "F", "black", "hardworking", 1);
+INSERT INTO bears VALUES (6,"Grinch", 21, "M", "white", "grumpy", 0);
+INSERT INTO bears VALUES (7,"Wendy", 9, "F", "brown", "joyful", 1);
+INSERT INTO bears VALUES (8,null, 56, "F", "white", "stubborn", 1);
